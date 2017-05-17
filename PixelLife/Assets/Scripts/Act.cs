edd251c4928ee098006c8bee9017a2e12d38232b
Jaswir <jaswirraghoe@gmail.com>
2017-05-17 18:05:06 +0200

    Startup

---
+++ PixelLife/Assets/Scripts/Act.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Act : MonoBehaviour
+{
+
+    public string name;
+    private bool cueActivated = false;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        CheckForCue();
+    }
+
+    void CheckForCue()
+    {
+        if (cueActivated)
+            return;
+
+        cueActivated = true;
+        PlayAct();
+    }
+
+    void PlayAct()
+    {
+        audioSource.Play();
+        EndAct();
+     
+    }
+
+
+    void EndAct()
+    {
+        Destroy(this, audioSource.clip.length + 0.3f);
+        Destroy(audioSource, audioSource.clip.length + 0.3f);
+    }
+}
---
+++ PixelLife/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+	        float moveHor = Input.GetAxis("Horizontal");
+	        float moveVer = Input.GetAxis("Vertical");
+	        Vector3 Movement = new Vector3(moveHor, moveVer, 0f);
+	        transform.position += Movement * Time.deltaTime * speed;
+	}
+}
